20caad28d210f9595b930e4b639f28922a5276e2
black.dev.x <thiago.dev.x.@gmail.com>
2021-10-30 19:07:41 -0300

    first commit

---
+++ src/views/examples/UseCallback.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+
+const UseCallback = props => {
+  return (
+    <div className='UseCallback'>
+      <PageTitle title='Hook UseCallback' subtitle='Retorna uma função memoizada!' />
+    </div>
+  )
+}
+
+export default UseCallback
