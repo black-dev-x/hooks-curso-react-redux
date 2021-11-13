86858c562b37b73e0dc7b4f2703d6b6746613ec3
black.dev.x <thiago.dev.x.@gmail.com>
2021-11-13 12:45:16 -0300

    use effect

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import SectionTitle from '../../components/layout/SectionTitle'
 import PageTitle from '../../components/layout/PageTitle'
 
-const UseEffect = props => {
+const factorialOf = number => {
+  if (number < 0) return 'Invalid'
+  if (number === 0) return 1
+  return number * factorialOf(number - 1)
+}
+const isEven = number => !number % 2
+const UseEffect = _ => {
+  const [number, setNumber] = useState(1)
+  const [factorial, setFactorial] = useState(1)
+  const [even, setEven] = useState(false)
+  useEffect(() => {
+    setFactorial(factorialOf(number))
+    setEven(isEven(number))
+  }, [number])
+
   return (
     <div className='UseEffect'>
-      <PageTitle title='Hook UseEffect' subtitle='Permite executar efeitos colaterais em componentes funcionais!' />
+      <PageTitle title='Hook UseEffect' subtitle='Collateral Effects!' />
+      <div className='center'>
+        <SectionTitle title='Exercise #01' />
+        <div>
+          <span className='text'>Factorial: </span>
+          <span className='text red'>{factorial}</span>
+        </div>
+        <input type='number' className='input' value={number} onChange={e => setNumber(+e.target.value)} />
+        <SectionTitle title='Exercise #02' />
+        <div>
+          <span className='text'>The number is: </span>
+          <span className='text red'>{even ? 'Even' : 'Odd'}</span>
+        </div>
+      </div>
     </div>
   )
 }
--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
+const merge = (value1, value2) => {
+  if (value1.length === 0) return value2
+  if (value2.length === 0) return value1
+  return value1.substring(0, 1) + value2.substring(0, 1) + merge(value1.substring(1), value2.substring(1))
+}
 const UseRef = props => {
+  const [mergedValue, setMergedValue] = useState('')
+  const [value1, setValue1] = useState('')
+  const [value2, setValue2] = useState('')
+  const input1 = useRef(null)
+  const input2 = useRef(null)
+  const counter = useRef(0)
+  useEffect(() => {
+    counter.current++
+    setMergedValue(merge(value1, value2))
+  }, [value1, value2])
+  counter.current++
   return (
     <div className='UseRef'>
-      <PageTitle title='Hook UseRef' subtitle='Retorna um objeto mutável com a propriedade .current!' />
+      <PageTitle title='Hook UseRef' subtitle='Returns a mutable object with the .current property!' />
+      <SectionTitle title='Exercise #01' />
+      <div className='center'>
+        <div>
+          <span className='text'>Value: </span>
+          <span className='text'>{mergedValue} </span>
+          <span className='text'>[</span>
+          <span className='text red'>{counter.current}</span>
+          <span className='text'>]</span>
+        </div>
+        <input ref={input1} type='text' className='input' value={value1} onChange={e => setValue1(e.target.value)} />
+      </div>
+      <SectionTitle title='Exercise - #02' />
+      <div className='center'>
+        <input ref={input2} type='text' className='input' value={value2} onChange={e => setValue2(e.target.value)} />
+      </div>
     </div>
   )
 }
