517f66e929a35ce2a966a06fa0f97adc3c8492c1
black.dev.x <thiago.dev.x.@gmail.com>
2021-11-23 17:12:41 -0300

    useReducer #01

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 const initialState = {
@@ -8,18 +8,32 @@ const initialState = {
   products: []
 }
 
-const reducer = (state = initialState, action) => {
+const reducer = (state, action) => {
   switch (action.type) {
     case 'add2ToNumber':
       return { ...state, number: state.number + 2 }
+    case 'login':
+      return { ...state, user: { name: action.payload } }
     default:
       return state
   }
 }
-const UseReducer = props => {
+const UseReducer = _ => {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div className='UseReducer'>
       <PageTitle title='Hook UseReducer' subtitle='Another way to use state in functional components!' />
+      <div className='center'>
+        <span className='text'>{state.user ? state.user.name : 'No user logged in'}</span>
+        <span className='text'></span>
+        <span className='text'>{state.number}</span>
+        <button className='btn' onClick={() => dispatch({ type: 'login', payload: 'Black Devx' })}>
+          Login
+        </button>
+        <button className='btn' onClick={() => dispatch({ type: 'add2ToNumber' })}>
+          +2
+        </button>
+      </div>
     </div>
   )
 }
